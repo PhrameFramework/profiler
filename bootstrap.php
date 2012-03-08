4dd74de23d9609871018648731b66e262a0d3b30
delmot <grebenjuk.s@gmail.com>
2012-03-09 00:11:22 +0200

    preparation for 0.3.0

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
  * Part of the Phrame
  *
  * @package    Profiler
- * @version    0.2.0
+ * @version    0.3.0
  * @author     Phrame Development Team
  * @license    MIT License
  * @copyright  2012 Phrame Development Team
